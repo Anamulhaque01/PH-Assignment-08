4f4fa260b60cb3cc0a7157cb728cfeba5a96f353
Anamulhaque01 <anamulhaque0357@gmail.com>
2026-05-03 14:54:22 +0600

    Login Essue Fixed

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,4 +1,6 @@
-// src/lib/auth.js
+import dns from "node:dns";
+dns.setServers(['8.8.8.8', '8.8.4.4']);
+
 import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
